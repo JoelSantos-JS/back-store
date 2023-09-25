efd84ee429c906569d3dde2091bfbe9f3f0664ef
joelsantos-js <joeltere9@gmail.com>
2023-09-25 15:16:16 -0300

    Initial commit

---
+++ src/main/java/com/joel/br/backstore/BackStoreApplication.java
@@ -0,0 +1,13 @@
+package com.joel.br.backstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/br/backstore/BackStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.backstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
